9514c900565bdeabba21bcfb1cc0f1dbbe51e3ec
piyush-shaw <piyushshaw0000@gmail.com>
2023-03-09 16:50:24 +0530

    [Piyush] added to retrieve all employee payroll in json server and called REST API

---
+++ RestSharpTest/RestSharpTest/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace RestSharpTest
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double Salary { get; set; }
+    }
+
+
+}
+
